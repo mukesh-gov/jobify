d5934fe29a81ce1baa59d1440278304a72587d91
Mukesh_Govardhanan <mukeshgwork@gmail.com>
2023-08-09 17:12:41 -0400

    Added more content to landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ad
+            laboriosam odio est quisquam suscipit pariatur. Incidunt culpa
+            quasi, nam fuga nihil sapiente quibusdam dolorem, eos voluptatibus
+            deleniti veniam repellendus?
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
