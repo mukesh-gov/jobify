37d28f1c4f8290ffc9f597cc8d7340977a79b8fc
Mukesh_Govardhanan <mukeshgwork@gmail.com>
2023-08-15 22:18:20 -0400

    Added Big Side bar

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,7 +1,26 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
+import { useDashboardContext } from "../pages/DashboardLayout";
 
 const BigSidebar = () => {
-  return <Wrapper>BigSideBar</Wrapper>;
+  const { showSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
